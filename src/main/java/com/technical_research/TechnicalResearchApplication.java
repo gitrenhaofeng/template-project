b8eee7dae323138aef36c203bd6d3cd3e7d041a9
任浩锋 <rhf01891978@alibaba-inc.com>
2026-05-06 14:41:02 +0800

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/technical_research/TechnicalResearchApplication.java
@@ -0,0 +1,13 @@
+package com.technical_research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalResearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalResearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/technical_research/TechnicalResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technical_research;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalResearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
